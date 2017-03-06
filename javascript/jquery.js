27c04104d3e6747bf04010262fa0e54d05792573
ashtine <ashtinet@yahoo.com>
2017-03-05 22:20:28 -0500

    Removed console.log

--- javascript/jquery.js
+++ javascript/jquery.js
@@ -2,21 +2,18 @@ $(document).ready(function() {
     
     $('.readmore').click(function(){
         event.preventDefault();
-        console.log('i clicked read more');
         $('.hide1').slideDown(800);
         $('.readmore').hide();
     });
 
     $('.readless').click(function() {
         event.preventDefault();
-        console.log('i clicked read less');
         $('.hide1').slideUp(800);
         $('.readmore').show();
     });
 
     $('.learnmore').click(function() {
         event.preventDefault();
-        console.log('i clicked learnmore');
         $('#learnmoretext').slideDown(800);
         $('.learnmore').hide();
     });
